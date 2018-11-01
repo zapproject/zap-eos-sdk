cbe642fc5a54f249a96d0e2a36178ab4e749d906
Kostya S <kostya.s@smartum.pro>
2018-11-01 14:49:06 +0300

    Dispatch package.

---
+++ packages/dispatch/src/dispatch.ts
@@ -0,0 +1,75 @@
+import * as Utils from "@zapjs/eos-utils";
+import {DispatchOptions} from "./types/types";
+
+export class Dispatch {
+    _account: Utils.Account;
+    _node: Utils.Node;
+    _zap_account: Utils.Account;
+
+    constructor({account, node}: DispatchOptions) {
+        this._account = account;
+        this._node = node;
+        this._zap_account = node.getZapAccount();
+    }
+
+    async connect() {
+        return await this._node.connect();
+    }
+
+    async query(provider: string, endpoint: string, query: string, onchain_provider: boolean) {
+        let eos = await this.connect();
+
+        return new Utils.Transaction()
+            .sender(this._account)
+            .receiver(this._zap_account)
+            .action('query')
+            .data({
+                subscriber: this._account,
+                provider: provider,
+                endpoint: endpoint,
+                query: query,
+                onchain_provider: onchain_provider ? 1 : 0,
+                onchain_subscriber: 0 // if we call it from js then it not onchain subscriber
+            })
+            .execute(eos);
+    }
+
+    async respond(id: number, params: string) {
+        let eos = await this.connect();
+
+        return new Utils.Transaction()
+            .sender(this._account)
+            .receiver(this._zap_account)
+            .action('respond')
+            .data({
+                responder: this._account,
+                id: id,
+                params: params
+            })
+            .execute(eos);
+    }
+
+    async queryQueriesInfo(from: number, to: number, limit: number) {
+        let eos = await this.connect();
+
+        return await eos.getTableRows(
+            true, // json
+            this._zap_account.name, // code
+            this._account.name, // scope
+            'qdata', // table name
+            'id', // table_key
+            from, // lower_bound
+            to, // upper_bound
+            limit, // limit
+            'i64', // key_type
+            1 // index position
+        );
+    }
+
+    listenQuries(callback?: Function) {
+        let listener = new Utils.SimpleEventListener(this._node.eos_config.httpEndpoint, 1)
+        listener.listen(callback, this._node.getZapAccount().name + '::bond');
+
+        return listener;
+    }
+}
---
+++ packages/dispatch/src/types/types.ts
@@ -0,0 +1,6 @@
+import * as Utils from "@zapjs/eos-utils";
+
+export type DispatchOptions = {
+    account: Utils.Account,
+    node: Utils.Node
+}
