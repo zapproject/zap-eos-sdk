8d4d83a6aa5a2217286c38f213fe8adcce784cdc
Your Name <you@example.com>
2019-04-06 16:50:27 +0200

    delete dist

--- packages/utils/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const account_1 = require("./account");
-const transaction_1 = require("./transaction");
-const eosnode_1 = require("./eosnode");
-exports.Account = account_1.Account;
-exports.Transaction = transaction_1.Transaction;
-exports.Node = eosnode_1.Node;
-//# sourceMappingURL=index.js.map
